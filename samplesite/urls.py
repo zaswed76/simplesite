6d473927b58f43fb782f3069b27f4e5ad16db63e
Сергей <zaswed76@gmail.com>
2020-02-24 22:34:09 +0200

    add urls

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bboard.views import index
 
 urlpatterns = [
+    path('bboard/', index),
     path('admin/', admin.site.urls),
 ]
